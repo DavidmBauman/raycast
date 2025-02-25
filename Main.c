1390260730ebfd2b5add50af1a4af021ce881b52
DavidmBauman <DBauman89@gmail.com>
2025-02-25 17:56:37 -0500

    SDL Window, Renderer setup

--- Main.c
+++ Main.c
@@ -1,10 +1,46 @@
 #include <stdio.h>
 #include <SDL.h>
+#include "Constants.h"
 
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+int initializeWindow() {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		fprintf(stderr, "Error initializing SDL.\n");
+		return FALSE;
+	}
+	window = SDL_CreateWindow(
+		"Raycast Engine", 
+		SDL_WINDOWPOS_CENTERED, 
+		SDL_WINDOWPOS_CENTERED, 
+		WINDOW_WIDTH, 
+		WINDOW_HEIGHT, 
+		SDL_WINDOW_BORDERLESS
+	);
+	if (!window) {
+		fprintf(stderr, "Error creating SDL window.\n");
+		return FALSE;
+	}									//-1 is default
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	if (!renderer) {
+		fprintf(stderr, "Error creating SDL renderer.\n");
+		return FALSE;
+	}
+	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+	return TRUE;
+}
 
 int main(int argc, char* args[]) {
-	 
+	initializeWindow();
 
+	setup();
+	
+	while (1) {
+		processInput();
+		update();
+		render();
+	}
 
 	return 0;
 }
